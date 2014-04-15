1c7804b22af2a53c8f3fb59d95f0c1fcd8e6c173
dangerousanimal <hideaki.ishii1204@gmail.com>
2014-04-15 23:57:06 +0900

    Adjust Gemfile and config.rb

--- Gemfile
+++ Gemfile
@@ -1,9 +1,20 @@
 # If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
+# the following line to use 'https'
 source 'http://rubygems.org'
 
-gem "middleman", "~> 3.3.2"
-gem "middleman-blog", "~> 3.5.2"
+gem 'middleman', '~> 3.3.2'
+gem 'middleman-blog', '~> 3.5.2'
+# For deploy
+gem 'middleman-deploy'
+
+# Using slim
+gem 'slim'
+
+# For markdown
+gem 'redcarpet'
+
+# For minify html
+gem 'middleman-minify-html'
 
 # For feed.xml.builder
-gem "builder", "~> 3.0"
+gem 'builder', '~> 3.0'
--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  blog.prefix = "blog"
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
@@ -84,13 +84,18 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :slim, { pretty: true, sort_attrs: false, format: :html5 }
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  # Minify HTML
+  activate :minify_html, remove_quotes: false, remove_intertag_spaces: true
 
   # Enable cache buster
   # activate :asset_hash
