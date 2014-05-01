f278528e680ea46c7a0415a23c963b6ae7945691
danimal141 <hideaki.ishii1204@gmail.com>
2014-05-02 00:12:15 +0900

    Prepare deploy

--- config.rb
+++ config.rb
@@ -26,6 +26,19 @@ activate :livereload
 activate :disqus do |d|
   d.shortname = 'danimal141'
 end
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'danimal141.net'
+  s3_sync.region                     = 'ap-northeast-1'
+  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY_ID'] || File.read("#{ENV['HOME']}/.danimal141/aws_access_key").strip
+  s3_sync.aws_secret_access_key      = ENV['AWS_SECRET_ACCESS_KEY'] || File.read("#{ENV['HOME']}/.danimal141/aws_secret_key").strip
+  s3_sync.delete                     = false
+  s3_sync.after_build                = false
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+end
 
 configure :development do
   activate :google_analytics do |ga|
@@ -36,7 +49,7 @@ end
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :asset_hash
+  #activate :asset_hash
   activate :minify_html, remove_quotes: false, remove_intertag_spaces: true
   activate :google_analytics do |ga|
     ga.tracking_id = 'UA-50475820-1'
