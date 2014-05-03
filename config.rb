1b43f289d1ce2c3641f03428e99001ff6f839bc3
danimal141 <hideaki.ishii1204@gmail.com>
2014-05-04 00:01:24 +0900

    Adjust social buttons

--- config.rb
+++ config.rb
@@ -31,7 +31,7 @@ activate :s3_sync do |s3_sync|
   s3_sync.region                     = 'ap-northeast-1'
   s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY_ID'] || File.read("#{ENV['HOME']}/.danimal141/aws_access_key").strip
   s3_sync.aws_secret_access_key      = ENV['AWS_SECRET_ACCESS_KEY'] || File.read("#{ENV['HOME']}/.danimal141/aws_secret_key").strip
-  s3_sync.delete                     = false
+  s3_sync.delete                     = true
   s3_sync.after_build                = false
   s3_sync.prefer_gzip                = true
   s3_sync.path_style                 = true
@@ -48,7 +48,7 @@ end
 
 configure :build do
   activate :minify_css
-  activate :minify_javascript
+  # activate :minify_javascript
   activate :minify_html, remove_quotes: false, remove_intertag_spaces: true
   activate :gzip
   activate :google_analytics do |ga|
